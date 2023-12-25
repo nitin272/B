df5b366b154bae5ea2f5b0994f8b10512115c98a
Nitin272 <142768664+nitin272@users.noreply.github.com>
2023-12-25 10:04:00 +0530

    Add files via upload

---
+++ client/src/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+         
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
